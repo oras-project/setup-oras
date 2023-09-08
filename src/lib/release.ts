1aa9a00ef8f909d797246299230fcb1c264c9c99
Billy Zha <jinzha1@microsoft.com>
2023-09-08 16:01:37 +0800

    refactor: revamp setup action with typescript (#18)
    This PR revamps setup action with typescript and 
    1) add a new job to verify `dist` folder complication
    2) update viable version to 1.1.0 and 1.0.1
    
    ---------
    
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

---
+++ src/lib/release.ts
@@ -0,0 +1,84 @@
+// Copyright The ORAS Authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as os from 'os';
+import releaseJson from './data/releases.json';
+
+// release is the type of official ORAS CLI release
+interface releases {
+    [version: string]: { 
+        [platform: string]: {
+            [arch: string]: {
+                checksum: string, 
+                url: string
+            } 
+        } 
+    }
+}
+
+// Get release info of a certain verion of ORAS CLI
+export function getReleaseInfo(version: string) {
+    const releases = releaseJson as releases;
+    if (!(version in releases)) {
+        console.log(`official ORAS CLI releases does not contain version ${version}`)
+        throw new Error(`official ORAS CLI releases does not contain version ${version}`);
+    }
+
+    const platform = mapPlatform();
+    const arch = mapArch();
+    const download = releases[version][platform][arch];
+    if (!download) {
+        throw new Error(`official ORAS CLI releases does not contain version ${version}, platform ${platform}, arch ${arch} is not supported`);
+    }
+    return download;
+}
+
+
+// getPlatform maps os.platform() to ORAS supported platforms.
+export function mapPlatform(): string {
+    const platform: string = os.platform();
+    switch (platform) {
+        case 'linux':
+            return 'linux';
+        case 'darwin':
+            return 'darwin';
+        case 'win32':
+            return 'windows';
+        default:
+            throw new Error(`unsupported platform: ${platform}`);
+    }
+}
+
+// mapArch maps os.arch() to ORAS supported architectures.
+export function mapArch(): string {
+    const architecture: string = os.arch();
+    switch (architecture) {
+        case 'x64':
+            return 'amd64';
+        case 'arm64':
+            return 'arm64';
+        case 'arm64':
+                return 'arm64';
+        case 's390':
+            return 's390x';
+        case 'arm':
+            return 'armv7';
+        default:
+            throw new Error(`unsupported architecture: ${architecture}`);
+    }
+}
+
+export function getBinaryExtension(): string {
+    const platform = mapPlatform();
+    return platform === 'windows' ? '.exe' : '';
+}
